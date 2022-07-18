6981c2841bbe38fc09a87a04184f197585fad8a2
la7ci9 <akarmiis997@gmail.com>
2022-07-18 19:51:15 +0200

    Starter project

---
+++ src/main/java/com/learn/KindOfCarMechanicApplication.java
@@ -0,0 +1,13 @@
+package com.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindOfCarMechanicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KindOfCarMechanicApplication.class, args);
+	}
+
+}

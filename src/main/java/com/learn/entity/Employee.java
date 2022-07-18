2539f46c9a8910d9f522eca1de2c6a79c12e7ed5
la7ci9 <akarmiis997@gmail.com>
2022-07-18 19:59:25 +0200

    basic employee entity

---
+++ src/main/java/com/learn/entity/Employee.java
@@ -0,0 +1,15 @@
+package com.learn.entity;
+
+public class Employee {
+	
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String email;
+	private String position; //mechanic or reception
+	
+	public Employee() {
+		
+	}
+}

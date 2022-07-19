d50dd1d2c778282628f8abe0163abd8cac081c92
la7ci9 <akarmiis997@gmail.com>
2022-07-19 12:49:23 +0200

    added annotations and created getter-setter,constuctors

--- src/main/java/com/learn/entity/Car.java
+++ src/main/java/com/learn/entity/Car.java
@@ -1,10 +1,72 @@
 package com.learn.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cars")
 public class Car {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String carBrand;
 	private String carModell;
 	private String plate;
 	private String status;
+	
+	public Car() {
+		//empty consturctor
+	}
+	
+	public Car(String carBrand, String carModell, String plate, String status) {
+		this.carBrand = carBrand;
+		this.carModell = carModell;
+		this.plate = plate;
+		this.status = status;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCarBrand() {
+		return carBrand;
+	}
+
+	public void setCarBrand(String carBrand) {
+		this.carBrand = carBrand;
+	}
+
+	public String getCarModell() {
+		return carModell;
+	}
+
+	public void setCarModell(String carModell) {
+		this.carModell = carModell;
+	}
+
+	public String getPlate() {
+		return plate;
+	}
+
+	public void setPlate(String plate) {
+		this.plate = plate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
 }
--- src/main/java/com/learn/entity/Employee.java
+++ src/main/java/com/learn/entity/Employee.java
@@ -1,7 +1,17 @@
 package com.learn.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String firstName;
 	private String lastName;
@@ -12,4 +22,63 @@ public class Employee {
 	public Employee() {
 		
 	}
+
+	public Employee(String firstName, String lastName, String phoneNumber, String email, String position) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.position = position;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	
+	
 }

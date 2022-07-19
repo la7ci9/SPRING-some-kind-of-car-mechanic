46e14b2484d3924e090d2b3d257533ba17797f15
la7ci9 <akarmiis997@gmail.com>
2022-07-19 15:06:59 +0200

    create methods

--- src/main/java/com/learn/service/EmployeeServiceDAO.java
+++ src/main/java/com/learn/service/EmployeeServiceDAO.java
@@ -1,5 +1,17 @@
 package com.learn.service;
 
-public interface EmployeeServiceDAO {
+import java.util.List;
+import com.learn.entity.Employee;
 
+public interface EmployeeServiceDAO {
+	
+	List<Employee> getAllEmployee();
+	
+	Employee saveEmployee(Employee emp);
+	
+	Employee getEmployeeById(Long id);
+	
+	Employee updateEmployee(Employee emp);
+
+	void deleteEmployee(Long id);
 }

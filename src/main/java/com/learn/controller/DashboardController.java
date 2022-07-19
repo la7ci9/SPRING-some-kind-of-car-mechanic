e9e9a526f6daec362ed4af4c1e6f4b5e2ffc0ee2
la7ci9 <akarmiis997@gmail.com>
2022-07-19 15:20:37 +0200

    home mapping creat

--- src/main/java/com/learn/controller/DashboardController.java
+++ src/main/java/com/learn/controller/DashboardController.java
@@ -1,8 +1,14 @@
 package com.learn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
+	
+	@GetMapping("/home")
+	public String homeDashboard() {
+		return "home_dash";
+	}
 
 }

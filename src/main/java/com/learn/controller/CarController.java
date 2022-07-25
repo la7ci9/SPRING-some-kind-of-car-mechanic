bf0bd4ff19519998735b79e1f06e5dd8ec25a2eb
la7ci9 <akarmiis997@gmail.com>
2022-07-25 20:43:43 +0200

    create new car and save

--- src/main/java/com/learn/controller/CarController.java
+++ src/main/java/com/learn/controller/CarController.java
@@ -1,8 +1,36 @@
 package com.learn.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.learn.entity.Car;
+import com.learn.service.CarService;
 
 @Controller
 public class CarController {
+	
+	@Autowired
+	private CarService carService;
+
+	public CarController(CarService carService) {
+		this.carService = carService;
+	}
+
+	@GetMapping("/add")
+	public String takeNewWork(Model model) {
+		Car car = new Car();
+		model.addAttribute("newcar", car);
+		return "new_work";
+	}
+	
+	@PostMapping("/home")
+	public String saveCar(@ModelAttribute("newcar") Car car) {
+		carService.saveCar(car);
+		return "redirect:/home";
+	}
 
 }

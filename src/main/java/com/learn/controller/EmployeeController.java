4f3fcc347fcb37da07e6b68f827102fd9ffe82a9
la7ci9 <akarmiis997@gmail.com>
2022-07-28 13:52:32 +0200

    list all employee method

--- src/main/java/com/learn/controller/EmployeeController.java
+++ src/main/java/com/learn/controller/EmployeeController.java
@@ -1,8 +1,24 @@
 package com.learn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.learn.service.EmployeeService;
 
 @Controller
 public class EmployeeController {
+	
+	private EmployeeService employeeService;
+	
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+	
+	@GetMapping("/employees")
+	public String employeesPage(Model model) {
+		model.addAttribute("employees", employeeService.getAllEmployee());
+		return "employees";
+	}
 
 }
